100ce9ec5b84861835962209fa474d09bde211a7
Akshay Rai <109916723+akshayrai14@users.noreply.github.com>
2024-09-06 06:10:02 +0000

    Update HelloWorldTest.java

--- src/test/java/com/mt/sample/test/HelloWorldTest.java
+++ src/test/java/com/mt/sample/test/HelloWorldTest.java
@@ -4,33 +4,17 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for HelloWorld.
- */
 public class HelloWorldTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
     public HelloWorldTest( String testName )
     {
         super( testName );
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( HelloWorldTest.class );
     }
-
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
     {
         assertTrue( true );
